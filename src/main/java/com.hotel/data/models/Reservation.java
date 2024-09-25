86a51fc3b4003f5c01b81832fc7f2f368022da5c
Hafeezco75 <hafeezco75@gmail.com>
2024-09-25 15:17:10 +0100

    Package setup created and dependencies resolved

---
+++ src/main/java/com.hotel/data/models/Reservation.java
@@ -0,0 +1,15 @@
+package data.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Reservation {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private long reservationId;
+
+
+}
